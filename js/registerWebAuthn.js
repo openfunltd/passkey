db1f8f53f93f2a5897ce59d4f8f4fe45d1075cb6
typingmonk <tmonk.hsieh@tmonk.tw>
2025-05-24 19:59:02 +0800

    Add client side code for passkey

---
+++ js/registerWebAuthn.js
@@ -0,0 +1,41 @@
+//Required urls: registerWebAuthnUrl, verifyWebAuthnRegistrationUrl
+
+function registerWebAuthn(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    $.post(registerWebAuthnUrl,
+      JSON.stringify(data),  
+      function(publicKeyOptions) {
+        createWebAuthnCredential(publicKeyOptions);
+      },
+      'json'
+    )
+}
+
+async function createWebAuthnCredential(publicKeyOptions) {
+  publicKeyOptions.challenge = base64toArrayBuffer(publicKeyOptions.challenge);
+  publicKeyOptions.user.id = base64toArrayBuffer(publicKeyOptions.user.id);
+
+  //create credentials using WebAuthn API
+  const credential = await navigator.credentials.create({
+    publicKey: publicKeyOptions
+  });
+
+  //prepare credentialData for sending to the server
+  const credentialData = {
+    id: credential.id,
+    rawId: arrayBufferToBase64(credential.rawId),
+    type: credential.type,
+    response: {
+      attestationObject: arrayBufferToBase64(credential.response.attestationObject),
+      clientDataJSON: arrayBufferToBase64(credential.response.clientDataJSON)
+    },
+    csrf_token: $('input[name="csrf_token"]').val(),
+  };
+
+  //send credentail to the server
+  sendCredentialToServer(verifyWebAuthnRegistrationUrl, credentialData);
+}
---
+++ js/requestWebAuthn.js
@@ -0,0 +1,42 @@
+//Required urls: requestWebAuthnUrl, verifyWebAuthnRegistrationUrl 
+
+function requestWebAuthn(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  $.post(requestWebAuthnUrl,
+    JSON.stringify(data),
+    getWebAuthnCredential,
+    'json'
+  );
+}
+
+async function getWebAuthnCredential(publicKeyOptions) {
+  publicKeyOptions.challenge = base64toArrayBuffer(publicKeyOptions.challenge);
+  if (publicKeyOptions.allowCredentials) {
+    for (let i = 0; i < publicKeyOptions.allowCredentials.length; i++) {
+      idArrayBuffer = base64toArrayBuffer(publicKeyOptions.allowCredentials[i].id);
+      publicKeyOptions.allowCredentials[i].id = idArrayBuffer;
+    }
+  }
+
+  const credential = await navigator.credentials.get({
+    publicKey: publicKeyOptions
+  });
+
+  const credentialData = {
+    id: credential.id,
+    rawId: arrayBufferToBase64(credential.rawId),
+    type: credential.type,
+    response: {
+      authenticatorData: arrayBufferToBase64(credential.response.authenticatorData),
+      clientDataJSON: arrayBufferToBase64(credential.response.clientDataJSON),
+      signature: arrayBufferToBase64(credential.response.signature)
+    },
+    csrf_token: $('input[name="csrf_token"]').val(),
+  };
+
+  sendCredentialToServer(verifyWebAuthnRequestUrl, credentialData);
+}
